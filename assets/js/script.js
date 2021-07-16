31843ed8588eb0a6bf960861da7a9b114eeb00ca
gcmartin <gscalica@gmail.com>
2021-07-16 14:32:36 -0700

    Add event listener to search button; will make an API request to PokeAPI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 // target search button
 let $searchBtn = document.getElementById("searchBtn");
 // add event listener to button
-$searchBtn.addEventListener("click", function(){
-  alert("You clicked on the search button!");
+$searchBtn.addEventListener("click", async function(){
+  // alert("You clicked on the search button!");
+  // when clicked, make an API call to the PokeAPI
+  // get the response...
+  let response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+  
+  // ...then extract data from the response...
+  let data = await response.json();
+
+  // ...do something with the data
+  console.log(data);
 });
