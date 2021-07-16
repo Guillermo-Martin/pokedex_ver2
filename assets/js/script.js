515fdbd66c2a96338e7adc55db9c5e759acc6d95
gcmartin <gscalica@gmail.com>
2021-07-16 13:31:45 -0700

    Create script.js file, add basic content to HTML, and connect button to javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// target search button
+let $searchBtn = document.getElementById("searchBtn");
+// add event listener to button
+$searchBtn.addEventListener("click", function(){
+  alert("You clicked on the search button!");
+});
