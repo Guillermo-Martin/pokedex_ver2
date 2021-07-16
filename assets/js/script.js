0237bf5218a4c9bbab454c57cc0ba81a84983740
gcmartin <gscalica@gmail.com>
2021-07-16 16:48:17 -0700

    Add data for displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ $searchBtn.addEventListener("click", async function(){
   // ...then extract data from the response...
   let data = await response.json();
 
+  console.log("your data", data);
+
   // ...do something with the data
   // make an API call to "pokemon-species" endpoint
   let speciesRes = await fetch(data.species.url);
@@ -16,13 +18,52 @@ $searchBtn.addEventListener("click", async function(){
   // ...extract data from the response...
   let speciesData = await speciesRes.json();
 
-  // 1.  ===== change the name =====
+  // console.log(speciesData.genera[7].genus)
+
+  // ===== change the id =====
+  // target the h4 for id
+  let $h2 = document.getElementById("pokemonId");
+  // change the text to be the id
+  $h2.textContent = data.id;
+
+  // ===== change the name =====
   // target the h3 for name
   let $h3 = document.getElementById("pokemonName");
   // change the text content to be the name
   $h3.textContent = data.name;
 
-  // 2.  ===== change picture of pokemon =====
+  // ===== change the genus =====
+  // target the h3 for genus
+  let $h3Genus = document.getElementById("pokemonGenus");
+  // change the text content to be the genus
+  $h3Genus.textContent = speciesData.genera[7].genus;
+
+  //  ===== create a ul to hold select info =====
+  let $ul = document.createElement("ul");
+  // create an li for type, height, weight, and description
+  let $typeLi = document.createElement("li");
+  let $heightLi = document.createElement("li");
+  let $weightLi = document.createElement("li");
+  let $descLi = document.createElement("li");
+  // add text to the li's
+
+  $typeLi.textContent = data.types[0].type.name;
+  $heightLi.textContent = "Height: " + data.height; // in decimeters
+  $weightLi.textContent = "Weight: " + data.weight; // in hectograms
+  
+  // append all li's to ul
+  $ul.appendChild($typeLi);
+  $ul.appendChild($heightLi);
+  $ul.appendChild($weightLi);
+  
+  // append ul with everything to displayDiv
+  // target displayDiv
+  let $displayDiv = document.getElementById("displayDiv");
+  // append $ul to $displayDiv
+  $displayDiv.appendChild($ul);
+
+  
+  // ===== change picture of pokemon =====
   // target the img tag
   let $pokemonImg = document.getElementById("pokemonImg");
   // change the src attribute to be the "front default sprite"
@@ -30,7 +71,7 @@ $searchBtn.addEventListener("click", async function(){
   // change the alt attribute to be the pokemon name
   $pokemonImg.setAttribute("alt", data.name);
 
-  // 3.  ===== change description of pokemon =====
+  // ===== change description of pokemon =====
   // target the description tag
   let $pokemonDesc = document.getElementById("pokemonDesc");
 
