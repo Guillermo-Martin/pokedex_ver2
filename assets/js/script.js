3f995b8f03afc8f9acf33d0bf2e6e84aaee96e16
gcmartin <gscalica@gmail.com>
2021-11-02 17:20:08 -0700

    Change timing of animations, clean up comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,15 @@ import { gsap } from './../../node_modules/gsap/index.js';
 let pokemonImgAnimation = gsap.from("#pokemonImg", { scale: 10, duration: 0.3, paused: true });
 let basicInfoAnimation = gsap.timeline();
 
-// target search button
+// target search button and add an event listener
 let $searchBtn = document.getElementById("searchBtn");
-// add event listener to button
 $searchBtn.addEventListener("click", async function(event){
   event.preventDefault();
 
   // 1.  get the value of the input (target the searchInput)
   let $searchInput = document.getElementById("searchInput");
 
-  // 2.  convert the input to all lowercase and save to variable, add to url
+  // 2.  convert the input to all lowercase and save to variable
   let searchPokemon = $searchInput.value.toLowerCase();
   
   // when clicked, make an API call to the PokeAPI - "pokemon" endpoint
@@ -33,55 +32,49 @@ $searchBtn.addEventListener("click", async function(event){
   // console.log(speciesData.genera[7].genus)
 
   // 6.  ===== change the id =====
-  // target the h4 for id
+  // target the h2, change text to be the id
   let $h2 = document.getElementById("pokemonId");
-  // change the text to be the id
   $h2.textContent = data.id;
 
   // 7.  ===== change the name =====
-  // target the h3 for name
+  // target the h3, change text to be the name
   let $h3 = document.getElementById("pokemonName");
-  // change the text content to be the name
   $h3.textContent = data.name;
 
   // 8.  ===== change the genus =====
-  // target the h3 for genus
+  // target the h3, change text to be the genus
   let $h3Genus = document.getElementById("pokemonGenus");
-  // change the text content to be the genus
   $h3Genus.textContent = speciesData.genera[7].genus;
 
   // 9.  ===== change info =====
-  // target the li's
+  // target the li's, change text to be the stats
   let $liType = document.getElementById("pokemonType");
   let $liHeight = document.getElementById("pokemonHeight");
   let $liWeight = document.getElementById("pokemonWeight");
-  // change their text
   $liType.textContent = "Type: " + data.types[0].type.name;
   $liHeight.textContent = "Height: " + data.height // in decimeters
   $liWeight.textContent = "Weight: " + data.weight // in hectograms
 
   // 10.  ===== change picture of pokemon =====
-  // target the img tag
+  // target the img tag, change src to be "front default sprite"; change alt to be name
   let $pokemonImg = document.getElementById("pokemonImg");
-  // change the src attribute to be the "front default sprite"
   $pokemonImg.setAttribute("src", data.sprites.front_default);
-  // change the alt attribute to be the pokemon name
   $pokemonImg.setAttribute("alt", data.name);
 
+  // ===== animations =====
   // when the pokemon image pops up, animate it
   pokemonImgAnimation.restart();
 
   // animate the ID and name
   basicInfoAnimation
     .from("#pokemonImg", { scale: 10, duration: 0.3, paused: true })
-    .from("#pokemon-basic-info span", {opacity: 0, stagger:0.3, duration:0.5}, "+=0.1")
+    .from("#pokemon-basic-info span", {opacity: 0, duration:0.3}, "+=0.5")
     .from("#pokemonGenus", {opacity: 0, duration:0.3}, "+=0.1")
     .from("#pokemonDesc", {opacity: 0, duration:0.3}, "+=0.1")
-    .from("ul li", {opacity: 0, stagger:0.5, duration:0.3}, "+=0.1")
+    .from("ul li", {opacity: 0, duration:0.3}, "+=0.1")
 
   // 11. ===== change description of pokemon =====
-  // target the description tag
+  // target the description tag, change text to be flavor text
   let $pokemonDesc = document.getElementById("pokemonDesc");
-  // change its text to be the flavor text
   $pokemonDesc.textContent = speciesData.flavor_text_entries[1].flavor_text;
 });
