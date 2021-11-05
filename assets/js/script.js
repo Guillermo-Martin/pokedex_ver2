ba5915fdcad51977d87f6fcc61a8656f16e144e2
gcmartin <gscalica@gmail.com>
2021-11-04 17:07:27 -0700

    Create 'pokemonTypes' object, add colors for each type, and have background color change depending on type

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,6 +68,56 @@ $searchBtn.addEventListener("click", async function(event){
       .to("#pokemonImg", {y: 0, duration: 0.09})
   });
 
+  // 11. ===== change description of pokemon =====
+  // target the description tag, change text to be flavor text
+  let $pokemonDesc = document.getElementById("pokemonDesc");
+  $pokemonDesc.textContent = speciesData.flavor_text_entries[1].flavor_text;
+
+
+  // --------------------------------------------------
+  // 12. check to see if a pokemon has one or two types
+  let typeOne = "";
+  let typeTwo = "";
+
+  if(data.types.length === 1) {
+    typeOne = data.types[0].type.name;
+  } else {
+    typeOne = data.types[0].type.name;
+    typeTwo = data.types[1].type.name;
+  }
+
+  // 13.  get the class name (color) for the pokemon type by looking in the object
+  let pokemonTypes = {
+    bug: "bug",
+    dark: "dark",
+    dragon: "dragon",
+    electric: "electric",
+    fairy: "fairy",
+    fighting: "fighting",
+    fire: "fire",
+    flying: "flying",
+    ghost: "ghost",
+    grass: "grass",
+    ground: "ground",
+    ice: "ice",
+    normal: "normal",
+    poison: "poison",
+    psychic: "psychic",
+    rock: "rock",
+    steel: "steel",
+    water: "water"
+  }
+
+  let typeOneColor = pokemonTypes[typeOne];
+  // let typeTwoColor = pokemonTypes[typeTwo];
+
+  // 14. target the body tag, add a class of typeOneColor/typeTwoColor
+  let $body = document.body;
+  $body.className = typeOneColor;
+
+  // --------------------------------------------------
+
+
   // ===== animations =====
   // when the pokemon image pops up, animate it
   pokemonImgAnimation.restart();
@@ -80,8 +130,5 @@ $searchBtn.addEventListener("click", async function(event){
     .from("#pokemonDesc", {opacity: 0, transform: "skewX(95deg)", duration:0.08}, "+=0.1")
     .from("ul li", {opacity: 0, transform: "skewX(95deg)", duration:0.08}, "+=0.1")
 
-  // 11. ===== change description of pokemon =====
-  // target the description tag, change text to be flavor text
-  let $pokemonDesc = document.getElementById("pokemonDesc");
-  $pokemonDesc.textContent = speciesData.flavor_text_entries[1].flavor_text;
+  
 });
