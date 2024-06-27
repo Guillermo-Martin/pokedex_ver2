048a31d932b64874b149aec8ad5b0e9ee32563b6
Guillermo Martin <gscalica@gmail.com>
2024-06-26 17:19:21 -0700

    Make API request to PokeAPI endpoint and get data

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,21 @@ console.log("connected!");
 // target the form
 let form = document.getElementById("form");
 
-// on submit, get the value of the input box
+// on submit, get the value of the input box, making an API request
 form.addEventListener("submit", (event) => {
   // prevent form default behavior
   event.preventDefault();
 
-  // input value
-  let searchInput = document.getElementById("searchInput").value;
+  // input value (API direct link uses lowercase letters)
+  let searchInput = document.getElementById("searchInput").value.toLowerCase();
 
-  console.log(searchInput);
+  // console.log(searchInput);
+
+  // make API request
+  let pokemonData = fetch(`https://pokeapi.co/api/v2/pokemon/${searchInput}`);
+
+  pokemonData
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
 });
-
-// console.log(form);
