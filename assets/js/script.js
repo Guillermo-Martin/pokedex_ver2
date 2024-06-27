9d55461e17ce3d98a2723205a276946c8e5c2b1f
Guillermo Martin <gscalica@gmail.com>
2024-06-26 18:06:39 -0700

    Change from using .then to async/await

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,23 @@ console.log("connected!");
 let form = document.getElementById("form");
 
 // on submit, get the value of the input box, making an API request
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   // prevent form default behavior
   event.preventDefault();
 
   // input value (API direct link uses lowercase letters)
   let searchInput = document.getElementById("searchInput").value.toLowerCase();
 
+  // see searchInput
   // console.log(searchInput);
 
-  // make API request
-  let pokemonData = fetch(`https://pokeapi.co/api/v2/pokemon/${searchInput}`);
+  // make API request and get the data
+  let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchInput}`);
 
-  pokemonData
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
+  // parse the data as JSON
+  let pokemonData = await response.json();
+
+  // see data
+  console.log(pokemonData);
 });
+
